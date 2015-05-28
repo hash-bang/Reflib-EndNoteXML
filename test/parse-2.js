3bd8e00368ee60ff2c46019fc75dc8a73e547116
Matt Carter <m@ttcarter.com>
2015-05-28 12:35:49 +1000

    Added second random sample set - tests seem to be passing

---
+++ test/parse-2.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var rl = require('../index');
+
+describe('EndNote XML parser - test #2', function() {
+	var resErr;
+	var data = {};
+
+	before(function(next) {
+		this.timeout(60 * 1000);
+		rl.parse(fs.readFileSync(__dirname + '/data/endnote2.xml'))
+			.on('error', function(err) {
+				resErr = err;
+				next();
+			})
+			.on('ref', function(ref) {
+				data[ref.isbn] = ref;
+			})
+			.on('end', function() {
+				next();
+			});
+	});
+
+	it('should not raise an error', function() {
+		expect(resErr).to.be.not.ok;
+	});
+
+	it('end count should be accurate', function() {
+		expect(Object.keys(data).length).to.equal(5);
+	});
+});
